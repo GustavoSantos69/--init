3bbfbcf5e52d3f8e10a38205b1efe72828943030
GustavoSantos69 <gustavolsantos2022@gmail.com>
2023-08-30 18:58:55 -0300

    teste

---
+++ python/aula_7.py
@@ -0,0 +1,17 @@
+'''
+Variável sõa usadas para salvar algo na memória do computador.
+PEP8: inicie variáveis com letra minúsculas, pode usar núemeros e underscore _ .
+O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variável).
+Uso: nome_variavel = expressão 
+'''
+nome_completo = 'Gustavo Lima dos Santos'
+soma = 10 + 9
+valor = bool("20")
+print(valor, type(valor))
+print('Nome:',nome_completo,'\nIdade:', soma, end=' anos\n\n')
+
+
+nome = 'Gustavo Lima dos santos'
+idade = 19
+maior_idade = idade >= 18
+print('nome:', nome, '\nidade:', idade,'\né maior de idade?', maior_idade)
